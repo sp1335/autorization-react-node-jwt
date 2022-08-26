597c91eae1ec56f1c5b7628804ff2c6937a574c4
wwiarenicz <wwiarenicz@gmail.com>
2022-08-26 17:14:34 +0200

    submit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect,useContext } from 'react';
 import LoginForm from './components/LoginForm';
-
+import {Context} from './index'
+import { observer } from 'mobx-react-lite';
 function App() {
+  const {store}=useContext(Context)
+  useEffect(()=>{
+    if(localStorage.getItem('token')){
+      store.checkAuth()
+    }
+  }, [])
   return (
     <>
+      <h1> {store.isAuth? `User logged in ${store.user.email}`:'SIGN IN'}</h1>
       <LoginForm/>
     </>
   );
 }
 
-export default App;
+export default observer(App);
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,4 +1,7 @@
+import axios from "axios";
 import { makeAutoObservable } from "mobx";
+import { API_URL } from "../http";
+import { AuthResponse } from "../models/response/AuthResponse";
 import { iUser } from "../models/response/iUser";
 import AuthService from "../service/AuthService";
 
@@ -17,13 +20,13 @@ export default class Store{
     async login(email: string, password: string){
         try {
             const response = await AuthService.login(email,password)
-            console.log(response)
+            console.log(response.data)
             localStorage.setItem('token', response.data.accessToken)
             this.setAuth(true)
             this.setUser(response.data.user)
         } catch (err) {
             if(err instanceof Error){
-                console.log(err)
+                console.log(err.message)
             }else{
                 console.log('Unexpected error',)
             }
@@ -32,13 +35,13 @@ export default class Store{
     async registration(email: string, password: string){
         try {
             const response = await AuthService.registration(email,password)
-            console.log(response)
+            console.log(response.data)
             localStorage.setItem('token', response.data.accessToken)
             this.setAuth(true)
             this.setUser(response.data.user)
         } catch (err) {
             if(err instanceof Error){
-                console.log(err)
+                console.log(err.message)
             }else{
                 console.log('Unexpected error',)
             }
@@ -47,13 +50,28 @@ export default class Store{
     async logout(){
         try {
             const response = await AuthService.logout()
-            console.log(response)
+            console.log(response.data)
             localStorage.removeItem('token')
             this.setAuth(false)
             this.setUser({} as iUser)
         } catch (err) {
             if(err instanceof Error){
-                console.log(err)
+                console.log(err.message)
+            }else{
+                console.log('Unexpected error',)
+            }
+        }
+    }
+    async checkAuth(){
+        try {
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials:true})
+            console.log(response)
+            localStorage.setItem('token', response.data.accessToken)
+            this.setAuth(true)
+            this.setUser(response.data.user)
+        } catch (err) {
+            if(err instanceof Error){
+                console.log(err.message)
             }else{
                 console.log('Unexpected error',)
             }
